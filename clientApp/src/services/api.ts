420d29d5b51f56ce642a44c014cc87eea22b7706
Dhiego Fernandes <dhiegofs1@gmail.com>
2025-11-29 18:49:36 -0300

    backend

--- clientApp/src/services/api.ts
+++ clientApp/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8083/",
+    baseURL: "https://agendeiback-fgbgdbbpchcebgh0.eastus2-01.azurewebsites.net/",
     headers: {
         "Content-Type": "application/json",
     },
