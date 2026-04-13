316477a6b2691c5b6e60f4ff8d71429e850974c6
Dhiego Fernandes <dhiegofs1@gmail.com>
2026-04-13 18:27:56 -0300

    Api env

--- clientApp/src/services/api.ts
+++ clientApp/src/services/api.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://agendeiback-fgbgdbbpchcebgh0.eastus2-01.azurewebsites.net/",
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
 })
 
-export default api;
+export default api;g
